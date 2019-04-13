d14b90b029f17cf28f9078e56d0eb1ae8ecbb9ae
BJ <q378648220@163.com>
2019-04-13 12:13:41 +0800

    asdf

--- scraper.py
+++ scraper.py
@@ -24,19 +24,12 @@
 # called "data".
 import requests
 from bs4 import BeautifulSoup
-import scraperwiki
 from datetime import datetime
 import pytz
-req = requests.get('http://www.shunon.com.hk/pricing.php')
+
+req = requests.get('https://osam.com/Commentary')
 soup = BeautifulSoup(req.content, 'html.parser')
-table = soup.find_all('table', {'class': 'Font16Boldc'})[1]
-element = table.find_all('p')[0]
-text = element.text.split()
-titles = [text[i] for i in (0, 2, 4, 6)]
-values = [int(text[i].replace('$', '').replace(',', ''))
-                  for i in (1, 3, 5, 7)]
-hkt = pytz.timezone('Asia/Hong_Kong')
-dt = datetime.now().replace(tzinfo=hkt).date()
-data = {titles[i]: values[i] for i in range(0, 4)}
-data['date'] = dt
-scraperwiki.sqlite.save(unique_keys=['date'], data=data)
+table = soup.select('a[href$=\".pdf\"]')
+
+data = [{"turl":"https://osam.com" + x['href']} for x in table]
+scraperwiki.sqlite.save(unique_keys=['turl'], data=data)
